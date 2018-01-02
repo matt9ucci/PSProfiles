7bfa85c1c393039f86fe8d7a9f1f7447098fdc5c
Matt Gucci <matt9ucci@gmail.com>
2018-01-02 12:20:24 +0900

    Add ipmo Docker completion modules

--- profile.ps1
+++ profile.ps1
@@ -28,6 +28,10 @@ $env:Path = @(
 	$env:Path
 ) -join ";"
 
+ipmo DockerCompletion -ea SilentlyContinue
+ipmo DockerComposeCompletion -ea SilentlyContinue
+ipmo DockerMachineCompletion -ea SilentlyContinue
+
 function prompt {
 	$Host.UI.RawUI.WindowTitle = "PS $($ExecutionContext.SessionState.Path.CurrentLocation)$('>' * ($NestedPromptLevel + 1))"
 	return 'PS> '

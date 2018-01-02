bef9320448054ac13464326a1426a4109c160b39
Matt Gucci <matt9ucci@gmail.com>
2018-01-02 13:34:40 +0900

    Add Get-Accelerator function to Core module

--- PSModules/Core/Core.psm1
+++ PSModules/Core/Core.psm1
@@ -9,5 +9,10 @@ function Grant-Administrator {
 function New-Directory([string[]]$Path) { New-Item $Path -Force -ItemType Directory }
 Set-Alias nd New-Directory
 
+function Get-Accelerator([string]$Name = '*') {
+	$accelerators = [powershell].Assembly.GetType('System.Management.Automation.TypeAccelerators')::Get
+	$accelerators.GetEnumerator() | Where-Object Key -Like $Name | Sort-Object -Property Key
+}
+
 function Edit-Hosts { saps notepad $env:SystemRoot\System32\drivers\etc\hosts -Verb RunAs }
 function Test-Administrator { ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator) }

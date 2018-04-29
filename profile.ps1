f4f51152c01501df974dfbb7a5348c54568ab478
Matt Gucci <matt9ucci@gmail.com>
2018-04-29 18:24:17 +0900

    Set SecurityProtocol = Tls12 if not SystemDefault

--- profile.ps1
+++ profile.ps1
@@ -32,6 +32,10 @@ ipmo DockerCompletion -ea SilentlyContinue
 ipmo DockerComposeCompletion -ea SilentlyContinue
 ipmo DockerMachineCompletion -ea SilentlyContinue
 
+if ([System.Net.ServicePointManager]::SecurityProtocol -ne [System.Net.SecurityProtocolType]::SystemDefault) {
+	[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
+}
+
 function prompt {
 	$Host.UI.RawUI.WindowTitle = "PS $($ExecutionContext.SessionState.Path.CurrentLocation)$('>' * ($NestedPromptLevel + 1))"
 	return 'PS> '

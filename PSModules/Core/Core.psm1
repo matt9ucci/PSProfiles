d98b8310341363da8ce1ec9c52edd7545437a2e0
Matt Gucci <matt9ucci@gmail.com>
2019-11-18 21:18:12 +0900

    Add function New-SymbolicLink to Core.psm1

--- PSModules/Core/Core.psm1
+++ PSModules/Core/Core.psm1
@@ -2,6 +2,23 @@
 . $PSScriptRoot\Command.ps1
 . $PSScriptRoot\Directory.ps1
 
+function New-SymbolicLink {
+	param (
+		[Parameter(Mandatory, Position = 0)]
+		[Alias('Target')]
+		[string]
+		$Value,
+
+		[string]
+		$Path = '.',
+
+		[string]
+		$Name = (Split-Path $Value -Leaf)
+	)
+
+	New-Item -Path $Path -Name $Name -Value $Value -ItemType SymbolicLink
+}
+
 function Get-Accelerator([string]$Name = '*') {
 	$accelerators = [powershell].Assembly.GetType('System.Management.Automation.TypeAccelerators')::Get
 	$accelerators.GetEnumerator() | Where-Object Key -Like $Name | Sort-Object -Property Key

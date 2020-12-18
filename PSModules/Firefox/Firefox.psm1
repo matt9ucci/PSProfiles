ec171f2e607c0846f497f3149116f04488cc0dd6
Matt Gucci <matt9ucci@gmail.com>
2020-12-18 22:22:30 +0900

    Add Firefox module

---
+++ PSModules/Firefox/Firefox.psm1
@@ -0,0 +1,29 @@
+Set-Variable FIREFOX_HOME "$env:ProgramFiles\Mozilla Firefox" -Scope Global -Option ReadOnly, AllScope
+Set-Variable FIREFOX_PROFILES "$env:APPDATA\Mozilla\Firefox\Profiles" -Scope Global -Option ReadOnly, AllScope
+
+function Get-FirefoxProfile($Name = '*') {
+	if ($IsLinux) {
+		Get-ChildItem -Directory $HOME/.mozilla/firefox -Filter *.$Name
+	} else {
+		Get-ChildItem -Directory $FIREFOX_PROFILES -Filter *.$Name
+	}
+}
+
+function Get-FirefoxUserJs([string]$Name) {
+	Join-Path (Get-FirefoxProfile $Name).FullName user.js
+}
+
+function Copy-FirefoxUserJs([string]$Name) {
+	$js = Join-Path $PSScriptRoot user.js
+	Copy-Item $js (Get-FirefoxUserJs $Name)
+}
+
+function Remove-FirefoxUserJs([string]$Name) {
+	Remove-Item (Get-FirefoxUserJs $Name)
+}
+
+function Uninstall-Firefox {
+	Start-Process -FilePath "$FIREFOX_HOME\uninstall\helper.exe" -ArgumentList '/S' -Wait
+	Remove-Item -Confirm -Recurse $env:APPDATA\Mozilla\Firefox
+	Remove-Item -Confirm -Recurse $env:LOCALAPPDATA\Mozilla\Firefox
+}
---
+++ PSModules/Firefox/user.js
@@ -0,0 +1,7 @@
+user_pref("accessibility.typeaheadfind.enablesound", false);
+user_pref("browser.aboutConfig.showWarning", false);
+user_pref("browser.search.widget.inNavBar", true);
+user_pref("browser.tabs.closeWindowWithLastTab", false);
+user_pref("browser.tabs.warnOnClose", false);
+user_pref("browser.tabs.warnOnCloseOtherTabs", false);
+user_pref("sidebar.position_start", false); // Move Sidebar to Right

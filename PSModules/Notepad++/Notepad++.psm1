e32959b8482a71cffcf5421e7b24712edbfdb255
Matt Gucci <matt9ucci@gmail.com>
2020-12-19 21:08:55 +0900

    Add Notepad++ module

---
+++ PSModules/Notepad++/Notepad++.psm1
@@ -0,0 +1,22 @@
+$NPP_HOME = "$USERAPPS/Notepad++"
+$NPP_EXE = Join-Path $NPP_HOME notepad++.exe
+
+function npp {
+	[string[]]$path = $args
+
+	$command = @($NPP_EXE)
+
+	if ((Test-Path $path -PathType Container) -contains $true) {
+		$command += '-openFoldersAsWorkspace'
+	}
+
+	# Resolve-Path is needed for "."
+	$command += (Resolve-Path $path).Path -join ' '
+
+	Write-Debug ('Invoking: {0}' -f ($command -join ' '))
+	Invoke-Expression -Command ($command -join ' ')
+}
+
+function Update-NppShortcutsXml {
+	Copy-Item $PSScriptRoot\shortcuts.xml (Join-Path $NPP_HOME shortcuts.xml)
+}

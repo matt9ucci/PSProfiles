099f44587c29d8732d58afe4d8ba0df4bbcebe9b
Matt Gucci <matt9ucci@gmail.com>
2021-04-11 15:51:09 +0900

    Remove Write-Host

--- PSScripts/Build-Wasmer.ps1
+++ PSScripts/Build-Wasmer.ps1
@@ -9,7 +9,7 @@ Push-Location wasmer
 
 cargo build --release
 New-Item $USERAPPS\Wasmer -ItemType Directory -Force | Out-Null
-Get-ChildItem .\target\release\ | Write-Host -ForegroundColor Green
+Get-ChildItem .\target\release\
 
 Move-Item .\target\release\wasmer.exe $USERAPPS\Wasmer
 New-Item -Path $USERAPPS\bin\Wasmer -Value $USERAPPS\Wasmer -ItemType Junction

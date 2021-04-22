0c2dcb3441c2ee728165259a7cdbcda241e9f1cf
Matt Gucci <matt9ucci@gmail.com>
2021-04-22 19:08:05 +0900

    Add DockerMultipass module

---
+++ PSModules/DockerMultipass/DockerMultipass.psm1
@@ -0,0 +1,48 @@
+function New-Instance {
+	param (
+		[Parameter(Position = 0, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+		[string[]]
+		$Name = 'DockerMultipass',
+
+		[ValidateRange(1, [UInt16]::MaxValue)]
+		[UInt16]
+		$Cpu = 2,
+
+		[ValidateRange(512MB, [UInt64]::MaxValue)]
+		[UInt64]
+		$Disk = 16GB,
+
+		[ValidateRange(128MB, [UInt64]::MaxValue)]
+		[UInt64]
+		$Memory = 2GB
+	)
+
+	$param = @(
+		'--cloud-init', "$PSScriptRoot/DockerMultipass.yaml"
+		'--name', $Name
+		'--cpus', $Cpu
+		'--disk', $Disk
+		'--mem', $Memory
+		'-vvv'
+	)
+
+	multipass launch @param
+}
+
+function New-DmContext {
+	param (
+		[string]
+		$Name = 'DockerMultipass'
+	)
+
+	$params = @(
+		'context'
+		'create'
+		"DM_$Name"
+		'--docker'
+		"host=ssh://ubuntu@$Name.mshome.net"
+		'--description'
+		'DockerMultipass'
+	)
+	docker @params
+}

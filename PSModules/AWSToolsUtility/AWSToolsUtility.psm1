196b3a6dfb6de8db408028377ee04865fd151241
Matt Gucci <matt9ucci@gmail.com>
2021-09-16 20:28:17 +0900

    Split AWSToolsUtility.psm1: Ec2.ps1

--- PSModules/AWSToolsUtility/AWSToolsUtility.psm1
+++ PSModules/AWSToolsUtility/AWSToolsUtility.psm1
@@ -3,6 +3,8 @@ if ($IsWindows) {
 	Set-Variable AWS_SHARED_CREDENTIAL_LOCATION "$HOME\.aws\credentials" -Option ReadOnly, AllScope -Scope Global -Force
 }
 
+. $PSScriptRoot\Ec2.ps1
+
 <#
 .LINK
 	Using AWS Credentials https://docs.aws.amazon.com/powershell/latest/userguide/specifying-your-aws-credentials.html
@@ -57,37 +59,6 @@ function Get-AwsToolsModuleName {
 	$moduleName -like $Name | sort
 }
 
-function Save-Ec2ConsoleScreenshot {
-	param (
-		[Parameter(Mandatory)]
-		[string]
-		$InstanceId,
-
-		[string]
-		$Path = '{0}-{1}.jpg' -f ('Ec2Console', (Get-Date -Format yyyyMMdd-HHmmss))
-	)
-
-	$imageData = Get-EC2ConsoleScreenshot -InstanceId $InstanceId -Select ImageData
-	[byte[]]$buffer = [System.Convert]::FromBase64String($imageData)
-	$stream = [System.IO.MemoryStream]::new($buffer)
-	$image = [System.Drawing.Image]::FromStream($stream)
-	$image.Save($Path, [System.Drawing.Imaging.ImageFormat]::Jpeg)
-}
-
-function Get-Ec2InstanceDescription {
-	Get-EC2Instance -Select Reservations.Instances
-}
-
-function Get-Ec2InstanceState {
-	Get-EC2Instance -Select Reservations.Instances | select @(
-		@{ n = 'InstanceName'; e = { ($_.Tags | ? Key -EQ Name).Value } }
-		'InstanceId'
-		@{ n = 'StateName'; e = { $_.State.Name } }
-		'StateReason'
-		'StateTransitionReason'
-	) | sort InstanceName
-}
-
 filter FromBase64ToUtf8 {
 	[System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($_))
 }
@@ -95,9 +66,6 @@ filter FromBase64ToUtf8 {
 filter FromUrlEncodedString {
 	[System.Web.HttpUtility]::UrlDecode($_)
 }
-
-sal ec2 Get-Ec2InstanceDescription
-sal ec2state Get-Ec2InstanceState
 
 Register-ArgumentCompleter -ParameterName Name -CommandName Install-AWSToolsModule -ScriptBlock {
 	param ($commandName, $parameterName, $wordToComplete)
---
+++ PSModules/AWSToolsUtility/Ec2.ps1
@@ -0,0 +1,33 @@
+function Get-Ec2InstanceDescription {
+	Get-EC2Instance -Select Reservations.Instances
+}
+
+function Get-Ec2InstanceState {
+	Get-EC2Instance -Select Reservations.Instances | select @(
+		@{ n = 'InstanceName'; e = { ($_.Tags | ? Key -EQ Name).Value } }
+		'InstanceId'
+		@{ n = 'StateName'; e = { $_.State.Name } }
+		'StateReason'
+		'StateTransitionReason'
+	) | sort InstanceName
+}
+
+function Save-Ec2ConsoleScreenshot {
+	param (
+		[Parameter(Mandatory)]
+		[string]
+		$InstanceId,
+
+		[string]
+		$Path = '{0}-{1}.jpg' -f ('Ec2Console', (Get-Date -Format yyyyMMdd-HHmmss))
+	)
+
+	$imageData = Get-EC2ConsoleScreenshot -InstanceId $InstanceId -Select ImageData
+	[byte[]]$buffer = [System.Convert]::FromBase64String($imageData)
+	$stream = [System.IO.MemoryStream]::new($buffer)
+	$image = [System.Drawing.Image]::FromStream($stream)
+	$image.Save($Path, [System.Drawing.Imaging.ImageFormat]::Jpeg)
+}
+
+sal Ec2 Get-Ec2InstanceDescription
+sal Ec2State Get-Ec2InstanceState

468c26619f5dad1ea49cfb74e1ba07a96ffdc2de
Matt Gucci <matt9ucci@gmail.com>
2022-12-19 17:10:04 +0900

    New module: Npm

---
+++ PSModules/Npm/Npm.psm1
@@ -0,0 +1,31 @@
+$env:npm_config_progress = 'false'
+
+if ($env:http_proxy) {
+	$env:npm_config_proxy = $env:http_proxy
+}
+if ($env:https_proxy) {
+	${env:npm_config_https-proxy} = $env:https_proxy
+}
+if ($env:no_proxy) {
+	$env:npm_config_noproxy = $env:no_proxy
+}
+
+Register-ArgumentCompleter -Native -CommandName npm -ScriptBlock {
+	param ($WordToComplete, $CommandAst, $CursorPosition)
+
+	$npmCommand = $CommandAst.CommandElements[1].Value
+
+	switch ($npmCommand) {
+		run {
+			$scriptsHashtable = (ConvertFrom-Json -InputObject (Get-Content -Path ./package.json -Raw) -AsHashtable).scripts
+			foreach ($scriptName in $scriptsHashtable.Keys -like "$WordToComplete*") {
+				[System.Management.Automation.CompletionResult]::new(
+					$scriptName,
+					$scriptName,
+					[System.Management.Automation.CompletionResultType]::Text,
+					$scriptsHashtable[$scriptName]
+				)
+			}
+		}
+	}
+}

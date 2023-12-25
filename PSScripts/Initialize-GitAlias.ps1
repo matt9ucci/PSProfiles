a267bbf94dd3e9f5db0bbeb680599b2ee9093b34
Matt Gucci <matt9ucci@gmail.com>
2023-12-25 21:48:20 +0900

    New script: Initialize-GitAlias

---
+++ PSScripts/Initialize-GitAlias.ps1
@@ -0,0 +1,6 @@
+git config --global alias.br branch
+git config --global alias.c commit
+git config --global alias.co checkout
+git config --global alias.l1 'log --oneline'
+git config --global alias.rb 'rebase -i'
+git config --global alias.st status
